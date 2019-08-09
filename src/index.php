be5161b2a330fe97abd346e0854ca309d56e02a5
crancurello <crancurello@gmail.com>
2019-08-09 12:26:36 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
                 <h2>GRACIAS TOTALES</h2>                                              
                 <h2>AGOSTO</h2>                                              
                 <p>Tu aplicacion PHP esta siendo ejecutada usando GitHub, CodePipeline, CodeBuild, CodeDeploy, ECR y ECS-Fargate.</p>
-                <p>Meetup Julio 2019</p>
+                <p>CompuSoluciones CICD completito!! YEAAAAH</p>
                 <br>
                 <br>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
