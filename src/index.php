fd99c3b697ad0bed8dd120a2c4ab935e7c3a091f
crancurello <crancurello@gmail.com>
2020-02-17 10:47:49 -0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,8 +16,8 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Aplicacion PHP con CI/CD</h1>
-                <h2>GRACIAS TOTALES</h2>                                              
-                <h2>AGOSTO</h2>                                              
+                <h2>Hola CompuSoluciones</h2>                                              
+                <h2>Febrero 2020</h2>                                              
                 <p>Tu aplicacion PHP esta siendo ejecutada usando GitHub, CodePipeline, CodeBuild, CodeDeploy, ECR y ECS-Fargate.</p>
                 <br>
                 <br>
