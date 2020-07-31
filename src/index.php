817d3c0df84df5aa6fa789f7c0f483991a7c4109
crancurello <crancurello@gmail.com>
2020-07-31 13:20:18 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Aplicacion PHP con CI/CD</h1>                                          
                 <h2>¡Bienvenidos Amigos!</h2>                                                              
-                <h2>21 de Abril del 2020</h2>                                              
+                <h2>31 de Julio del 2020</h2>                                              
                 <p>Tu aplicacion PHP esta siendo ejecutada usando GitHub, CodePipeline, CodeBuild, CodeDeploy, ECR y ECS-Fargate.</p>
                 <br>
                 <br>
